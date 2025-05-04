b9c45f125ec4e1349de1799ba03f41b6ea196dfb
martinotamendit <martin.otamendit@alumno.buap.mx>
2025-05-04 04:11:50 -0600

    create gen_recipe method

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import json
+import typing_extensions as typing
 import google.generativeai as genai
 from dotenv import load_dotenv
 from flask import Flask, request, render_template, session, jsonify
@@ -14,20 +16,49 @@ GEMINI_API_KEY = os.environ['GEMINI_API_KEY']
 #Configuración de Gemini.
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-2.0-flash")
-prompt = 'You are a professional chef. Generate a recipe based on the ingredients and quantities provided by the user.\n\nIngredients:\n{{#each ingredients}}- {{this.name}}: {{this.quantity}}\n{{/each}}\n\nRecipe Name: (Suggest a creative and appropriate name for the recipe.)\nInstructions: (Provide detailed, step-by-step instructions for preparing the recipe.)'
+gen_config = genai.types.GenerationConfig(
+    temperature = 0.8,
+    response_mime_type='application/json',
+)
 
 @app.route('/')
 def home():
     #if "history" not in session:
         #session["history"] = []
-    response = model.generate_content(prompt)
-    return jsonify(response.text), 200
+    return jsonify("Hola Mundo!!!"), 200
 
-@app.route('/gen-recipe', methods=['POST'])
+#Método para generar recetas.
+@app.route('/gen_recipe', methods=['POST'])
 def gen_recipe():
-    response = model.generate_content(prompt)
-    print(response.text)
-    return jsonify(response.text), 200
+    #Construcción de prompt.
+    ingredients = request.json.get('ingredients', [])
+    prompt = f"Eres un chef profesional. Genera una receta basada en los ingredientes y las cantidades proporcionadas.\n\nIngredientes:\n"
+    for ingredient in ingredients:
+        prompt += f"- {ingredient['name']}: {ingredient['quantity']}\n"
+    
+    prompt += "\nGenera la salida en el idioma proporcionado de los ingredientes con el siguiente formato:\nrecipe_name: (Sugiere un nombre creativo y apropiado para la receta.)\ningredients:(Proporciona la lista de ingredientes utilizados en la receta con un array de cadenas)\ninstructions: (Proporciona instrucciones detalladas, paso a paso y numeradas, para preparar la receta.)\nrecommendations:(Proporciona una lista de recomendaciones/notas/consejos a tomar en cuenta para preparar la receta.)"
+    try:
+        response = model.generate_content(contents=prompt, generation_config=gen_config)
+        recipe_json = json.loads(response.text)
+        #print(recipe_json)
+
+        recipe_name = recipe_json.get("recipe_name")
+        final_ingredients = recipe_json.get("ingredients")
+        instructions = recipe_json.get("instructions")
+        recommendations = recipe_json.get("recommendations")
+
+        
+
+        return jsonify({
+            "recipeName": recipe_name,
+            "ingredients": final_ingredients,
+            "instructions": instructions,
+            "recommendations": recommendations
+        })
+    
+    except Exception as e:
+        print(f"Ocurrió un error: {e}\nRespuesta del modelo:\n{+response.text}")
+        return jsonify({"Error": "Could not generate recipe"}), 500
 
 
 if __name__ == '__main__':
